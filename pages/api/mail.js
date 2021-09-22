65fae4873f70679256aafee79aa612c30d87cf07
Charlie Edwards <charles.edwards1811@gmail.com>
2021-09-22 16:45:05 +1000

    added sendgrid

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,41 +1,15 @@
-import nodemailer from "nodemailer";
+import sgMail from '@sendgrid/mail';
+sgMail.setApiKey(process.env.SENDGRID_API)
 
 export default async function handler(req, res) {
   const { name, email, subject, message, mobile } = JSON.parse(req.body);
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      type: "OAuth2",
-      user: process.env.GOOGLE_EMAIL,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH,
-      accessToken: process.env.GOOGLE_ACCESS,
-      expires: 1484314697598,
-    },
-  });
-  const mail = {
-    from: `"${name}" <${email}>`,
+  const msg = { 
     to: process.env.EMAIL_TO,
-    subject: subject,
-    text:
-      message +
-      `\n\nregards, ${name}` +
-      `\nemail: ${email}` +
-      `\nmobile: ${mobile}`,
-  };
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mail, (err, info) => {
-      if (err) {
-        reject(err)
-        res.json({ success: false, statusMessage: "Message failed" });
-      } else { 
-        resolve(info)
-        res.json({ success: true, statusMessage: "Message successfully sent!" });
-      }
-    });
-    
-  });
+    from: `"${name}" <${email}>`,
+    subject,
+    text: message
+  }
+  const sent = await sgMail.send(msg)
+  console.log(sent)
+  res.json(sent)
 }
