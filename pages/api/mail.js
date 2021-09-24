bd9db85287dcb9d913ff3833820c9697959fe5ec
Charlie Edwards <charles.edwards1811@gmail.com>
2021-09-24 12:23:24 +1000

    send grid configured

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,15 +1,19 @@
-import sgMail from '@sendgrid/mail';
-sgMail.setApiKey(process.env.SENDGRID_API)
+import sgMail from "@sendgrid/mail";
+sgMail.setApiKey(process.env.SENDGRID_API);
 
 export default async function handler(req, res) {
-  const { name, email, subject, message, mobile } = JSON.parse(req.body);
-  const msg = { 
-    to: process.env.EMAIL_TO,
-    from: `"${name}" <${email}>`,
-    subject,
-    text: message
+  try {
+    const { name, email, subject, message, mobile } = JSON.parse(req.body);
+    const text = `${message} \nmobile: ${mobile} \nemail: ${email}`;
+    const msg = {
+      to: process.env.EMAIL_TO,
+      from: `"${name}" <${process.env.GOOGLE_EMAIL}>`,
+      subject,
+      text,
+    };
+    const sent = await sgMail.send(msg);
+    res.json(sent);
+  } catch {
+    res.json(e);
   }
-  const sent = await sgMail.send(msg)
-  console.log(sent)
-  res.json(sent)
 }
