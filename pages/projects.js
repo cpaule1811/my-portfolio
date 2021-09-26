f8e0b24b609553ca28313ae11363d831928d332c
Charlie Edwards <charles.edwards1811@gmail.com>
2021-09-26 12:31:44 +1000

    no effect on swiper

--- pages/projects.js
+++ pages/projects.js
@@ -1,25 +1,18 @@
 import Card from "../components/card";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, EffectCube } from "swiper";
+import { Navigation, Pagination } from "swiper";
 import { projectData } from "../utils/projects-data";
 
 export default function Projects() {
   return (
     <div className="carousel">
       <Swiper
-        modules={[Navigation, Pagination, EffectCube]}
-        effect="cube"
+        modules={[Navigation, Pagination]}
         spaceBetween={0}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
-        cubeEffect={{
-          shadow: false,
-          shadowOffset: -5,
-          shadowScale: 1,
-          slideShadows: false,
-        }}
-        loop={true} 
+        loop={true}
       >
         {projectData.map((item, i) => {
           return (
